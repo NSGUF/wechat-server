abedc630c69896ac944d72b1ace3a8f1a607c631
NSGUF <1968083780@qq.com>
2019-11-14 23:08:19 +0800

    完善readme和配置

---
+++ README.md
@@ -0,0 +1,7 @@
+# wechat-server
+> 仿微信聊天后端
+# 技术栈
+> koa2 + koa2各种中间件 + ts + mongodb + socket.io
+
+# 注意
+1. 使用前记得改src/config里面的配置；
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,8 @@
-const ip = '47.103.2.26'
+const ip = 'localhost'
+
 const port = 8080
 const dbPath = `mongodb://${ip}/wechat-mongoose`
 const ipAndPort = `http://${ip}:${port}`
-// export const config: any = {
-//     ip: ,
-//     // ip: '47.103.2.26',
-//     port: 8080,
-//     dbPath: `mongodb://${this.ip}/wechat-mongoose`,
-//     ipAndPort: `http://${this.ip}:'+${this.dbPath}`,
-// }
 
 export {
     ip,
